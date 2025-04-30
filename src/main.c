fc2ff3d18cabbd8d7b017366df1208856740386e
ali sadeghi far <sadeghifarali9@gmail.com>
2025-04-30 23:15:15 +0330

    fix

--- src/main.c
+++ src/main.c
@@ -363,11 +363,12 @@ HttpResponse *user_agent_view(HttpRequest request){
 HttpResponse *files_view(HttpRequest request){
 	char header[1024];
 	char *fileName = request.arguments[0];
-	FILE *file =fopen(fileName,"r");
+	char filePath[1204];
+	snprintf(filePath,sizeof(fileName)+sizeof("./"),"./%s",fileName);
+	printf("File: [%s]",filePath);
+	FILE *file =fopen(filePath,"r");
 	long body_size = 0;
 	char *body;
-
-
 
 	if(file != NULL){
 
