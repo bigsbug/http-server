8cd7d1bb58297cc91f6dc4e405bc14f613a43d91
ali sadeghi far <sadeghifarali9@gmail.com>
2025-05-01 21:40:56 +0330

    reset base path

--- src/main.c
+++ src/main.c
@@ -420,7 +420,7 @@ HttpResponse *files_view(HttpRequest request){
 HttpResponse *post_files_view(HttpRequest request){
 	char header[1024];
 	char *fileName = request.arguments[0];
-	char *fileBasePath = "./";
+	char *fileBasePath = "/tmp/data/codecrafters.io/http-server-tester/";
 	int fileFullPathLength = strlen(fileName)+strlen(fileBasePath);
 	char fileFullPath[fileFullPathLength +1];
 	printf("Content: [%s]\n",request.body);
